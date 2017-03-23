14c243265ad662c8fdf3b79b78fd8f769e648009
Mattias Gees <mattias.gees@gmail.com>
2017-03-23 18:14:45 +0100

    Add extra environment

--- variables_env.tf
+++ variables_env.tf
@@ -3,13 +3,15 @@ variable "cidr_block" {
   default = {
     staging = "10.201.0.0/16"
     default = "10.200.0.0/16"
+    production = "10.201.0.0/16"
   }
 }
 
 variable "sg_count" {
   type = "map"
   default = {
-    staging = "3"
+    staging = "2"
     default = "2"
+    production = "3"
   }
 }
